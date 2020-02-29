ca780427327e946e00e5a083c6a8f7c0479db983
Matt <36577952+MattsCurrie@users.noreply.github.com>
2020-02-29 11:30:28 -0800

    Add files via upload
    Added more comments to explain and give examples of what I mean.

--- src/Interpretter.java
+++ src/Interpretter.java
@@ -4,12 +4,44 @@ package botPackage;
 
 public class Interpretter {
 	
-	public static MessageType interpret(String msg) {
+	public static MessageType interpretType(String msg) {
 		/* feeds string to DialogFlow to determine what the user intends
 		 * If DialogFlow decides Intention is a Statement -> return MessageType.Statement
 		 * else if Question -> return MessageType.Question
 		 * else Other
 		 */
 		return null;
+	}
+
+	public static String[] getKeywords(String msg) {
+		// returns a list of the Key Entities of a sentence (Pronounds and Verbs)
+		
+		/* EXAMPLE
+		 * Input: "I feel like running away"
+		 * 	 - Keywords: I [@Pronoun], feel [@Verb] 
+		 * 			+ these keywords get identified by DialogFlow
+		 * 			+ if feasable, then we can store "like running away" to be used in possible response
+		 * 
+		 */
+		return null;
+		
+		/* END GOAL OF OBTAINING THESE KEY WORDS
+		 * Input: String msg
+		 * 		+ EX: "I feel like running away"
+		 *  - keywords identified
+		 *  - keywords transformed ("I" -> "you")
+		 *  - keywords get slotted into a script 
+		 *  	+ EX: "Why do ___[@Pronoun] ______[@Verb] _____[excess]"
+		 *  - transformed keywords get slotted into script
+		 *  	+ EX: "Why do you feel like running away"
+		 *  Output: String reply
+		 *  	+ EX: "Why do you feel like running away"
+		 */
+	}
+	
+	public static boolean checkForX (String msg, String x) {
+		// here we could check if the string x is present in the message
+		
+		return msg.contains(x); // x could be phrases that indicate self harm
 	}
 }
--- src/Message.java
+++ src/Message.java
@@ -3,9 +3,12 @@ package botPackage;
 public class Message {
 	String msg;
 	MessageType type; // gets decided by interpretter [DialogFlow]
+	// may need to be broken up into their DialogFlow entities (verbs and pronouns)
+	String[] keywords; 
 	
 	public Message(String msg) {
 		this.msg = msg;
-		this.type = Interpretter.interpret(msg);
+		this.type = Interpretter.interpretType(msg);
+		this.keywords = Interpretter.getKeywords(msg);
 	}
 }
