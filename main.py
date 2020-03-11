769fc02d366c30bb8d14df4504f465bd82aa9638
Matt <36577952+MattsCurrie@users.noreply.github.com>
2020-03-11 10:44:14 -0700

    Add files via upload
    Refactored code used in conversation with the bot when the conversation screenshots were taken.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
 session_client = dialogflow.SessionsClient()
 session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
 
-
+# connects with DialogFlow Agent to get responses
 def getresponse(text, lang_code, sech):
     text_input = dialogflow.types.TextInput(language_code=lang_code, text=text)
     query_input = dialogflow.types.QueryInput(text=text_input)
@@ -33,7 +33,7 @@ def getresponse(text, lang_code, sech):
     except InvalidArgument:
         raise
     # print("Query text:", response.query_result.query_text)
-    print("Detected intent:", response.query_result.intent.display_name)
+    # print("Detected intent:", response.query_result.intent.display_name)
     # print("Detected intent confidence:", response.query_result.intent_detection_confidence)
     if not response.query_result.fulfillment_text:
         print("Dr. Azile: Tell me more")
@@ -41,30 +41,27 @@ def getresponse(text, lang_code, sech):
         # print("Fulfillment text:", response.query_result.fulfillment_text)
         print("Dr. Azile: ", response.query_result.fulfillment_text)
 
+# filler intro conversation
+def introConversation(printString):
+    print(printString)
+    input("You: ")
 
-print("Say hi to get started. Say bye at anytime to end the conversation.")
-text_to_be_analyzed = input("You: ")
-print("Formal Disclaimer that this is a chatbot and not a professional help bot for people.")
-print("Hello my name is Dr. Azile, I am here to help with your psychological needs. Please know that I am not a real "
-      "person; I am a chat bot, created by some amazing people to help you. If you need a real person or I feel you "
-      "need someone real I can give you resources like a helpline number.")
-print("Dr. Azile: To get started I have a few question about you. Just let me know when you are ready")
-text_to_be_analyzed = input("You: ")
-print("Dr. Azile: How old are you?")
-age = input("You: ")
-print("Dr. Azile: What is your name?")
-name = input("You: ")
-print("Dr. Azile: Are you currently a elementary school student, middle school student, high school student, "
+
+introConversation("Say hi to get started")
+introConversation("Formal Disclaimer that this is a chatbot and not a professional help bot for people." +
+      "\nHello my name is Dr. Azile, I am here to help with your psychological needs. Please know that I am not a real " +
+      "person; I am a chat bot, created by some amazing people to help you. If you need a real person or I feel you " +
+      "need someone real I can give you resources like a helpline number." +
+      "\nDr. Azile: To get started I have a few question about you. Just let me know when you are ready")
+introConversation("Dr. Azile: How old are you?")
+introConversation("Dr. Azile: What is your name?")
+introConversation("Dr. Azile: Are you currently a elementary school student, middle school student, high school student, "
       "univeristy school student, or not a student?")
-isStudent = input("You: ")
-print("Dr. Azile: Do you currently work full time, part time, or not at all?")
-work = input("You: ")
-print("Dr. Azile: On a scale of 1 to 5 with one being terrible and five being great, how are you feeling currently?")
-feel = input("You: ")
-print("Dr. Azile: My focus is anxiety. On a scale of 1 to 5 with one being no anxiety and 5 being a anxiety attack,"
+introConversation("Dr. Azile: On a scale of 1 to 5 with one being terrible and five being great, how are you feeling currently?")
+introConversation("Dr. Azile: My focus is anxiety. On a scale of 1 to 5 with one being no anxiety and 5 being a anxiety attack,"
       " how anxious are you feeling today?")
-anxiety = input("You: ")
 print("Dr. Azile: Ok lets get started. Tell me about how you are feeling today.")
+
 
 done = False
 while not done:
