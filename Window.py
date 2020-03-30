88231a480361e042a22fdb1a0323f743a5155f64
KatrinaMart <k.martel@outlook.com>
2020-03-30 14:27:29 -0700

    Add files via upload
    Updated GUI with Matt and Jeff. Update includes:
    - Making the window resizable in width, but not height
    - Adjusting text widget width and height, as well as wrapping the text so that words don't get cut off
    - Adding a border around the text widget
    - Anchoring the text widget and frame
    - A function so that the user hits the return key in the "Entry" widget to display their message, rather than a reliance on the button
    - Changed "speak" to "Send" for the text of the button, and added a foreground colour.

--- Window.py
+++ Window.py
@@ -1,8 +1,11 @@
+import tkinter
 from tkinter import *
 
 from main import do_thing
 
-window = Tk()
+window = tkinter.Tk()
+window.resizable(width=True, height=False)
+window.title("Azile Chatbot") #displays at the top window bar
 scrollbar = Scrollbar(window)
 scrollbar.pack(side=RIGHT, fill=Y)
 user = StringVar()
@@ -10,9 +13,11 @@ bot = StringVar()
 
 num = 0
 
-messages = Text(window, width=50, height=20, yscrollcommand=scrollbar.set)
+messages = Text(window, width=120, height=20, yscrollcommand=scrollbar.set, wrap=WORD)
+messages.config(borderwidth=5,highlightcolor="LightBlue4",
+               background = "LightBlue3", relief=SUNKEN)
 scrollbar.config(command=messages.yview())
-messages.pack()
+messages.pack(anchor='nw', expand=1, fill=BOTH)
 
 
 def Enter_pressed():
@@ -30,14 +35,17 @@ def Enter_pressed():
     # label.pack()
     return "break"
 
+def event_enter(key):
+    Enter_pressed()
+
 
 Label(window, text=" user : ").pack(side=LEFT)
-Entry(window, textvariable=user, width=35).pack(side=LEFT)
+Entry(window, textvariable=user, width=10, background="floral white").pack(side=LEFT, anchor='nw', expand=1, fill=BOTH)
 frame = Frame(window)  # , width=300, height=300)
-Button(window, text="speak", command=Enter_pressed).pack(side=LEFT)
+Button(window, width=10, text="Send", foreground = "turquoise4", command=Enter_pressed, relief = "raised").pack(side=LEFT)
 messages.insert(END, "Dr. Azile: " + '%s\n' % "Say hi to get started")
-#input_field.bind("<Return>", Enter_pressed) #here??
-frame.pack()
+window.bind("<Return>", event_enter) #here??
+frame.pack(anchor='nw', expand=1, fill=BOTH)
 
 window.mainloop()
 
